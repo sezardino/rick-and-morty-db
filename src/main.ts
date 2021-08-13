7c215c323bc972e446dadffec736659058b8708b
Edward <e.arechwa@gmail.com>
2021-08-13 12:53:56 +0200

    feat: add initial scss structure

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,16 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+import '@/assets/styles/index.scss'
+
+import Layouts from "./Layouts";
+
+const app = createApp(App);
+
+Layouts.forEach((layout) => {
+  app.component(layout.name, layout);
+});
+
+app.use(store);
+app.use(router);
+app.mount("#app");

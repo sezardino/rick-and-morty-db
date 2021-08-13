f42cc94f5253059d77cfbd4c85328ff67088830c
Edward <e.arechwa@gmail.com>
2021-08-13 20:10:44 +0200

    feat: home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,27 @@
 <template>
-  <my-table></my-table>
-  <my-pagination></my-pagination>
+  <section>
+    <h1 class="visually-hidden">All Characters</h1>
+    <my-table :data="all"></my-table>
+    <my-pagination></my-pagination>
+  </section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { mapGetters } from "vuex";
 
 export default defineComponent({
   name: "Home",
   components: {},
+  computed: {
+    ...mapGetters({ all: "characters/all" }),
+  },
+  async mounted() {
+    try {
+      await this.$store.dispatch("characters/getAllCharacters");
+    } catch (error) {
+      console.log(error);
+    }
+  },
 });
 </script>

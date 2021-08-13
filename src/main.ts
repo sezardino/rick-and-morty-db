46f1b964427c08a1c0b5ffbe2f00f3df795c24df
Edward <e.arechwa@gmail.com>
2021-08-13 21:14:38 +0200

    add comments to main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,21 @@
+// vue
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// styles
 import "@/assets/styles/index.scss";
 
+// components
 import Layouts from "@/layouts";
 import UIComponents from "@/components/UI";
 import Icons from "@/components/icons";
 
+// create app
 const app = createApp(App);
 
+// register components/layouts/icons
 Layouts.forEach((layout) => {
   app.component(layout.name, layout);
 });
@@ -21,6 +26,7 @@ Icons.forEach((icon) => {
   app.component(icon.name, icon);
 });
 
+// register mixins/plugins/ mount app
 app.use(store);
 app.use(router);
 app.mount("#app");

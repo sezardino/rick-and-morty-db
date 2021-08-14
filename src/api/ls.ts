9ad0b51801d9bc5760d10a67d13b7316e5ce2dd8
Edward <e.arechwa@gmail.com>
2021-08-14 21:17:01 +0200

    feat: add local storage

--- src/api/ls.ts
+++ src/api/ls.ts
@@ -1,3 +1,21 @@
-class LSApi {}
+import { ICharacter } from "@/interfaces";
+
+class LSApi {
+  keyName: string;
+
+  constructor() {
+    this.keyName = "rANDmDB";
+  }
+
+  getData() {
+    const data = localStorage.getItem(this.keyName);
+    return JSON.parse(data!) || null;
+  }
+
+  saveData(items: ICharacter[]) {
+    const data = JSON.stringify(items);
+    localStorage.setItem(this.keyName, data);
+  }
+}
 
 export default new LSApi();

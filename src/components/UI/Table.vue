8f0843ee4a5bb61c852b866d5c63dd04697d8465
Edward <e.arechwa@gmail.com>
2021-08-14 18:38:16 +0200

    feat: add no data case

--- src/components/UI/Table.vue
+++ src/components/UI/Table.vue
@@ -1,45 +1,48 @@
 <template>
-  <table class="table">
-    <thead class="table__head">
-      <tr class="table__row table__row--head">
-        <th class="table__data--empty"></th>
-        <th class="table__head-data">Photo</th>
-        <th class="table__head-data">Character ID</th>
-        <th class="table__head-data">Name</th>
-        <th class="table__head-data">Gender</th>
-        <th class="table__head-data">Species</th>
-        <th class="table__head-data">Last Episode</th>
-        <th class="table__head-data">Add To Favorites</th>
-        <th class="table__data--empty"></th>
-      </tr>
-    </thead>
-    <tbody class="table__body">
-      <tr class="table__row" v-for="item in data" :key="item.id">
-        <td class="table__data--empty"></td>
-        <td class="table__data">
-          <img class="table__img" :src="item.image" :alt="item.name" />
-        </td>
-        <td class="table__data">{{ item.id }}</td>
-        <td class="table__data">{{ item.name }}</td>
-        <td class="table__data">
-          <component
-            :is="genderIcon(item.gender)"
-            class="table__icon"
-          ></component>
-          {{ item.gender }}
-        </td>
-        <td class="table__data">{{ item.species }}</td>
-        <td class="table__data">{{ lastEpisode(item.episode) }}</td>
-        <td class="table__data">
-          <button class="button table__button">
-            <star-icon></star-icon>
-            <span class="visually-hidden"> Add to favorites </span>
-          </button>
-        </td>
-        <td class="table__data--empty"></td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="table">
+    <p class="table__info" v-if="!data.length">{{ emptyLabel }}</p>
+    <table class="table__table" v-else>
+      <thead class="table__head">
+        <tr class="table__row table__row--head">
+          <th class="table__data--empty"></th>
+          <th class="table__head-data">Photo</th>
+          <th class="table__head-data">Character ID</th>
+          <th class="table__head-data">Name</th>
+          <th class="table__head-data">Gender</th>
+          <th class="table__head-data">Species</th>
+          <th class="table__head-data">Last Episode</th>
+          <th class="table__head-data">Add To Favorites</th>
+          <th class="table__data--empty"></th>
+        </tr>
+      </thead>
+      <tbody class="table__body">
+        <tr class="table__row" v-for="item in data" :key="item.id">
+          <td class="table__data--empty"></td>
+          <td class="table__data">
+            <img class="table__img" :src="item.image" :alt="item.name" />
+          </td>
+          <td class="table__data">{{ item.id }}</td>
+          <td class="table__data">{{ item.name }}</td>
+          <td class="table__data">
+            <component
+              :is="genderIcon(item.gender)"
+              class="table__icon"
+            ></component>
+            {{ item.gender }}
+          </td>
+          <td class="table__data">{{ item.species }}</td>
+          <td class="table__data">{{ lastEpisode(item.episode) }}</td>
+          <td class="table__data">
+            <button class="button table__button">
+              <star-icon></star-icon>
+              <span class="visually-hidden"> Add to favorites </span>
+            </button>
+          </td>
+          <td class="table__data--empty"></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script lang="ts">
@@ -51,6 +54,7 @@ export default defineComponent({
   name: "my-table",
   props: {
     data: Array,
+    emptyLabel: { type: String, required: true },
   },
 
   methods: {

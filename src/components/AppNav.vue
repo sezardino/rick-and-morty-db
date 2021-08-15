3a649b5e6351980bb5b8f928960ed9bb094f1d03
Edward <e.arechwa@gmail.com>
2021-08-15 12:48:12 +0200

    refactor: refactor app nav

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -1,18 +1,15 @@
 <template>
   <nav class="nav">
     <ul class="nav__list">
-      <li class="nav__item">
-        <router-link to="/" active-class="nav__link--active" class="nav__link">
-          All Characters
-        </router-link>
-      </li>
-      <li class="nav__item">
+      <li class="nav__item" v-for="link in links" :key="link.path">
         <router-link
-          to="/favorites"
-          active-class="nav__link--active"
-          class="nav__link"
+          :to="link.path"
+          :class="{
+            nav__link: true,
+            'nav__link--active': $route.name === link.name,
+          }"
         >
-          Favorites
+          {{ link.name }}
         </router-link>
       </li>
     </ul>
@@ -24,8 +21,18 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "app-nav",
-  // setup() {
+  computed: {
+    links() {
+      const routes = this.$router.options.routes.filter(
+        (route) => route.meta?.inNav
+      );
+      const neededRoutes = routes.map((item) => {
+        item.path = item.path.replace(":page", "");
+        return item;
+      });
 
-  // },
+      return neededRoutes;
+    },
+  },
 });
 </script>

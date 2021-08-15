d0dbb71d5d1a1fca8ec16b5d3ea1599f06b2fa6d
Edward <e.arechwa@gmail.com>
2021-08-15 12:38:06 +0200

    feat: create error boundary

---
+++ src/components/AppErrorBoundary.vue
@@ -0,0 +1,13 @@
+<template>
+  <my-error v-if="error"></my-error>
+  <slot v-else></slot>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "app-error-boundary",
+  props: { error: { type: Boolean, required: true } },
+});
+</script>

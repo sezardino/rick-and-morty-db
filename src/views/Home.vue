4e7703f000959d0fe120c7ae30aa9fd676343cb9
Edward <e.arechwa@gmail.com>
2021-08-15 20:12:44 +0200

    feat: start with themes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,1 +1,46 @@
-<template>HOME PAGE</template>
+<template>
+  <section class="home">
+    <h1 class="visually-hidden">Home Page</h1>
+    <div class="themes home__themes">
+      <h2 class="themes__title">Thoose theme</h2>
+      <ul class="themes__list">
+        <li class="themes__item">
+          <button class="themes__button">
+            <img
+              src="@/assets/images/dark.png"
+              alt="Portal theme"
+              class="themes__img"
+            />
+          </button>
+        </li>
+        <li class="themes__item">
+          <button class="themes__button">
+            <img
+              src="@/assets/images/portal.png"
+              alt="Jarry theme"
+              class="themes__img"
+            />
+          </button>
+        </li>
+        <li class="themes__item">
+          <button class="themes__button">
+            <img
+              src="@/assets/images/rick.png"
+              alt="Rick theme"
+              class="themes__img"
+            />
+          </button>
+        </li>
+        <li class="themes__item">
+          <button class="themes__button">
+            <img
+              src="@/assets/images/head.png"
+              alt="Rick theme"
+              class="themes__img"
+            />
+          </button>
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>

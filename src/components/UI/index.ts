274eeaa7a3bdb4a95e8871c676fc567a11a460ec
Edward <e.arechwa@gmail.com>
2021-08-15 12:48:51 +0200

    feat: add error component to ui

--- src/components/UI/index.ts
+++ src/components/UI/index.ts
@@ -1,5 +1,6 @@
 import Loader from "./Loader.vue";
 import Pagination from "./Pagination.vue";
 import Table from "./Table.vue";
+import Error from "./Error.vue";
 
-export default [Loader, Pagination, Table];
+export default [Loader, Pagination, Table, Error];
